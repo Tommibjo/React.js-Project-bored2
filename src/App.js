b453e0553cff4b3dc5713f94569128a6ccfb71e3
Tommibjo <tommi.bjrklund@gmail.com>
2019-07-11 22:50:28 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerx">
+      <div className="containery">
+        <div className="PersonalDetails">
+          <div className="Name">TOMMI BJÖRKLUND</div>
+        <br></br>
+          <div className="Title">web developer</div>
+        </div>
+        <h2></h2>
+        <div className="content">
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
